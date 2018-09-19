ef353890c84f931817a8c126b30640137d4ef402
Daniil Dvoryanov <d.dvoryanov@innopolis.ru>
2018-09-19 22:11:08 +0300

    Exercise 2 completed (added locks).

--- Lab5/ex2.sh
+++ Lab5/ex2.sh
@@ -4,7 +4,13 @@ a=0
 
 while [ $a -lt 10 ]
 do
-    number=$(tail -n 1 test.txt)
-    echo $((number + 1)) >> test.txt 
+    while ! ln ex2_out.txt ex2_out.txt.lock 2>/dev/null
+    do
+        sleep 1
+    done
+    number=$(tail -n 1 ex2_out.txt)
+    echo $((number + 1)) >> ex2_out.txt 
+    rm -f ex2_out.txt.lock
+
     a=$((a + 1))
 done
