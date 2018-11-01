01289117d07d3e9c6164efd0dc8f9dae4eebe9b4
Daniil Dvoryanov <d.dvoryanov@innopolis.ru>
2018-11-01 22:23:15 +0300

    Fixed segmentation fault in fourth task.

--- week11/ex4.c
+++ week11/ex4.c
@@ -6,17 +6,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define size 17
+#define size 16
 
 int main()
 {
     int from = open("ex1.txt", O_RDONLY);
-    int to = open("ex1.memcpy.txt", O_WRONLY);
+    int to = open("ex1.memcpy.txt", O_RDWR);
 
     ftruncate(to, size);
 
     char *addr_from = mmap(NULL, size, PROT_READ, MAP_SHARED, from, 0);
-    char *addr_to = mmap(NULL, size, PROT_WRITE, MAP_SHARED, to, 0);
+    char *addr_to = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, to, 0);
     
     memcpy(addr_to, addr_from, size);
 
