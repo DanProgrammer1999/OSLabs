43e21afda239c0c3884f84b58f74fb537cd6ffd8
Daniil Dvoryanov <d.dvoryanov@innopolis.ru>
2018-11-01 22:49:58 +0300

    Implemented a function to discover the size of the file automatically.

--- week11/ex4.c
+++ week11/ex4.c
@@ -6,19 +6,28 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define size 16
+int filesize(int fd)
+{
+    struct stat st;
+    fstat(fd, &st);
+
+    return st.st_size;
+}
 
 int main()
 {
     int from = open("ex1.txt", O_RDONLY);
     int to = open("ex1.memcpy.txt", O_RDWR);
 
+    int size = filesize(from);
     ftruncate(to, size);
 
     char *addr_from = mmap(NULL, size, PROT_READ, MAP_SHARED, from, 0);
     char *addr_to = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, to, 0);
-    
+
     memcpy(addr_to, addr_from, size);
 
+    munmap(addr_from, size);
+    munmap(addr_to, size);
     return 0;
 }
